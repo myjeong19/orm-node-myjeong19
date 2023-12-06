73ee68a52b84567434de6a5c7224150b304fc5f5
myjeong19@naver.com <myjeong19@naver.com>
2023-12-06 17:57:20 +0900

    Design. 회원 약관 페이지

--- DAY7/noderoutingapp/routes/member.js
+++ DAY7/noderoutingapp/routes/member.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get('/join', (req, res) => res.render('member/join'));
 router.get('/entry', (req, res) => res.render('member/entry'));
 router.post('/entry', function (req, res) {
-  res.redirect('http://localhost:3000/member/join');
+  res.redirect('/main');
 });
 
 module.exports = router;
