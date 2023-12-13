0070c43beb44a8f8282f58b0da0799e3343a6bb4
myjeong19@naver.com <myjeong19@naver.com>
2023-12-13 15:20:34 +0900

    feat. router, add, modify, delete

--- DAY12/nodechatapp/routes/index.js
+++ DAY12/nodechatapp/routes/index.js
@@ -10,18 +10,24 @@ router.get('/login', async (req, res, next) => {
   res.render('login');
 });
 
-router.post('/login', async (req, res) => {});
+router.post('/login', async (req, res) => {
+  res.redirect('/chat');
+});
 
 router.get('/entry', async (req, res, next) => {
   res.render('entry');
 });
 
-router.post('/entry', async (req, res) => {});
+router.post('/entry', async (req, res) => {
+  res.redirect('login');
+});
 
 router.get('/find', async (req, res, next) => {
   res.render('find');
 });
 
-router.post('/find', async (req, res) => {});
+router.post('/find', async (req, res) => {
+  res.redirect('login');
+});
 
 module.exports = router;
