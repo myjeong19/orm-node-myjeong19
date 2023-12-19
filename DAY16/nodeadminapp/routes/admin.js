adbb87159fb5e34f20578c923b8418b8ec9ade78
myjeong19@naver.com <myjeong19@naver.com>
2023-12-19 21:33:31 +0900

    Feat. 관리자 페이지

--- DAY16/nodeadminapp/routes/admin.js
+++ DAY16/nodeadminapp/routes/admin.js
@@ -3,17 +3,32 @@ const router = express.Router();
 
 const adminList = [
   {
-    admin_member_id: '1',
+    admin_member_id: '0',
     company_code: '1',
-    admin_id: 'myjeong19@naver.com',
+    admin_id: 'myjeong19',
     admin_password: '1234',
     admin_name: '정민영',
     email: 'myjeong19@naver.com',
     telephone: '010-3957-3540',
     used_yn_code: '1',
-    reg_user_id: Date.now(),
-    edit_user: 'ADMIN',
-    edit_date: Date.now(),
+    edit_user: 'admin',
+    edit_date: new Date(),
+    reg_user_id: 'admin',
+    reg_date: '2023-12-18',
+  },
+
+  {
+    admin_member_id: '1',
+    company_code: '2',
+    admin_id: 'hyun-do_kim',
+    admin_password: '1234',
+    admin_name: '김현도',
+    email: 'hyun-do_kim@naver.com',
+    telephone: '010-1111-1111',
+    used_yn_code: '1', // 사용 여부
+    edit_user: 'hyun-do_kim',
+    edit_date: new Date(),
+    reg_user_id: 'hyun-do_kim',
     reg_date: '2023-12-18',
   },
 ];
@@ -38,6 +53,7 @@ router.get('/delete', async (req, res) => {
 
 router.get('/modify/:id', async (req, res) => {
   const admin_id = req.params.id;
+
   const admin = adminList[admin_id];
 
   res.render('admin/modify', { admin });
